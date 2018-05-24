72fba008523eb46731980f28e44a191fd4c45ad3
dioonata <dioonatacv@gmail.com>
2018-05-24 16:57:58 -0300

    Primeiro

---
+++ src/fundamentosDaLinguagem/LojaFerramentas.java
@@ -0,0 +1,29 @@
+package fundamentosDaLinguagem;
+
+import java.util.Scanner;
+
+public class LojaFerramentas {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int codigoProduto = 0;
+
+        do {
+
+            System.out.println("Digite o codigo do produto: ");
+            codigoProduto = entrada.nextInt();
+
+            if (codigoProduto != 0) {
+                String corredor = (codigoProduto % 2 == 0) ? "\"direira\"" : "\"esquerda\"";
+
+                for (int i = 8; i > 1; i--) {
+                    if (codigoProduto % i == 0) {
+                        System.out.println("O código do produto: " + codigoProduto + " fica no corredor da " + corredor + " e na gaveta " + i);
+                    }
+                }
+            }
+        } while (codigoProduto != 0);
+    }
+
+}
